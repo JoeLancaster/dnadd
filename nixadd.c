19c385c4149bdfa99ac9b1da39f1bb657d09e54c
zakkor <edward.partenie@gmail.com>
2020-09-13 19:41:43 +0300

    Add backup file swapping logic

--- nixadd.c
+++ nixadd.c
@@ -17,13 +17,13 @@ int main(int argc, char **argv)
 {
 	if (argc < 2) {
 		printf
-		    ("Please specify the program name as the first argument\n");
+		    ("please specify the program name as the first argument\n");
 		exit(EXIT_FAILURE);
 	}
 	char *package = argv[1];
 
 	FILE *fp = fopen("./configuration.nix", "r");
-	FILE *dfp = fopen("./configuration.nixadd.nix", "w");
+	FILE *dfp = fopen("./configuration.nixadd-backup.nix", "w");
 	if (fp == NULL || dfp == NULL)
 		exit(EXIT_FAILURE);
 
@@ -34,6 +34,19 @@ int main(int argc, char **argv)
 		if (strcmp(t, MARKER) == 0) {
 			fprintf(dfp, "%s\n", package);
 		}
+	}
+	// Swap files
+	if (rename("./configuration.nix", "./.nixadd.tmp") != 0) {
+		fprintf(stderr, "can't rename configuration.nix");
+		exit(EXIT_FAILURE);
+	}
+	if (rename("./configuration.nixadd-backup.nix", "./configuration.nix") != 0) {
+		fprintf(stderr, "can't rename configuration.nix");
+		exit(EXIT_FAILURE);
+	}
+	if (rename("./.nixadd.tmp", "./configuration.nixadd-backup.nix") != 0) {
+		fprintf(stderr, "can't rename configuration.nix");
+		exit(EXIT_FAILURE);
 	}
 	fclose(fp);
 	fclose(dfp);
