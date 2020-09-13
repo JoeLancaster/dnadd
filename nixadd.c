ac3c182ed327c6e2e15554f9b9e547f0e337f95c
Joe Lancaster <joe.a.lancas@gmail.com>
2020-09-13 19:46:07 +0100

    add robust path handling

--- nixadd.c
+++ nixadd.c
@@ -3,6 +3,11 @@
 #include <ctype.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
+#include <limits.h>
+#include <libgen.h>
+
+#define DEBUG
 
 #define BUFLEN 255
 #define MARKER "environment.systemPackages = with pkgs; [\n"
@@ -18,8 +23,15 @@ char *ltrim(char *s)
 
 int main(int argc, char **argv)
 {
+	int eno;
 	int exit_usage = 0;
 	int opt;
+#ifdef DEBUG
+	char *_config_path = "./configuration.nix";
+	fprintf(stderr, "%s", "DEBUG BUILD\n");
+#else
+	char *_config_path = "/etc/nixos/configuration.nix";	//default config location for NixOS
+#endif
 
 	while ((opt = getopt(argc, argv, "h")) != -1) {
 		switch (opt) {
@@ -33,11 +45,60 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 	char *package = argv[optind];
+	char *cfg_full_path;
+	char *path_buf = malloc(PATH_MAX);
+	char *path_cpy = malloc(PATH_MAX);
+	char *path_tmp;
+	char *fname_tmp;
 
-	FILE *fp = fopen("./configuration.nix", "r");
-	FILE *dfp = fopen("./configuration.nixadd-backup.nix", "w");
-	if (fp == NULL || dfp == NULL)
+	errno = 0;
+	realpath(_config_path, path_buf);
+	cfg_full_path = strdup(path_buf);
+	eno = errno;
+	int enorp = eno;
+	errno = 0;
+	FILE *fp = fopen(path_buf, "r");
+	eno = errno;
+	//use _config_path instead of path_buf in failure in case it was realpath that failed
+
+	if (eno || enorp) {
+		if (eno == EACCES || enorp == EACCES) {
+			fprintf(stderr, "Access denied for %s. (Are you sudo?)\n", _config_path);
+		} else {
+			fprintf(stderr, "Error opening file %s\n", _config_path);
+		}
+		fclose(fp);
 		exit(EXIT_FAILURE);
+	}
+	strcpy(path_cpy, path_buf);
+
+	char *cfg_file_name;
+	char *cfg_dir;
+
+	path_tmp = dirname(path_buf);	//[dir/base]name may modify its arg so we make use of the original (since it will be overwritten later) and the copy
+	fname_tmp = basename(path_cpy);
+
+	cfg_file_name = strdup(fname_tmp);	//strdup mallocs!
+	cfg_dir = strdup(path_tmp);
+
+	char *backup_file_path = malloc(PATH_MAX);	//the name of the backup file (to be created)
+	strcpy(backup_file_path, cfg_full_path);
+	strcat(backup_file_path, ".nixadd");
+
+	char *temp_path = malloc(PATH_MAX);	//a temp name for swapping
+	strcpy(temp_path, cfg_dir);
+	strcat(temp_path, "/");
+	strcat(temp_path, ".nixtemp");
+
+	//we shouldn't need to use realpath for the backup file path.
+
+	errno = 0;
+	FILE *dfp = fopen(backup_file_path, "w");
+	eno = errno;
+	if (eno) {
+		fprintf(stderr, "Error with %s: %s\n", backup_file_path, strerror(eno));
+		exit(EXIT_FAILURE);
+	}
 
 	char buffer[BUFLEN];
 	while (fgets(buffer, BUFLEN, fp)) {
@@ -47,20 +108,25 @@ int main(int argc, char **argv)
 			fprintf(dfp, "%s\n", package);
 		}
 	}
-	// Swap files
-	if (rename("./configuration.nix", "./.nixadd.tmp") != 0) {
+
+	//Swap files
+	if (rename(cfg_full_path, temp_path) != 0) {
 		fprintf(stderr, "can't rename configuration.nix");
 		exit(EXIT_FAILURE);
 	}
-	if (rename("./configuration.nixadd-backup.nix", "./configuration.nix")
-	    != 0) {
+	if (rename(backup_file_path, cfg_full_path) != 0) {
 		fprintf(stderr, "can't rename configuration.nix");
 		exit(EXIT_FAILURE);
 	}
-	if (rename("./.nixadd.tmp", "./configuration.nixadd-backup.nix") != 0) {
+	if (rename(temp_path, backup_file_path) != 0) {
 		fprintf(stderr, "can't rename configuration.nix");
 		exit(EXIT_FAILURE);
 	}
+	printf("Successfully edited %s\n", cfg_file_name);	//just to suppress warning for now
+	free(temp_path);
+
+	free(path_buf);
+	free(path_cpy);
 	fclose(fp);
 	fclose(dfp);
 	return 0;
