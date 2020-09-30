9803e2767dec7db141f1132f0c70d38103eb76b2
Joe Lancaster <joe.a.lancas@gmail.com>
2020-09-30 16:40:59 +0100

    add missing rename.c

---
+++ rename.c
@@ -0,0 +1,27 @@
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "rename.h"
+
+void swap_names (char * dst, char * src, char * tmp) {
+  int eno;
+  errno = 0;
+  rename(src, tmp);
+  eno = errno;
+  if (eno) {
+    puts(strerror(eno));
+  }
+  errno = 0;
+  rename(dst, src);
+  eno = errno;
+  if (eno) {
+    puts(strerror(eno));
+  }
+  errno = 0;
+  rename(tmp, dst);
+  eno = errno;
+  if (eno) {
+    puts(strerror(eno));
+  }
+}
